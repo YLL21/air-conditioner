412965257e7b80eac3f9f9f8ad015899bf275a7e
YL <56504151+YLL21@users.noreply.github.com>
2021-08-29 17:46:07 +0800

    Update Copyright.tsx

--- src/layouts/Copyright.tsx
+++ src/layouts/Copyright.tsx
@@ -15,90 +15,16 @@ import { mdiWechat } from "@mdi/js";
 import { mdiEarth } from "@mdi/js";
 import { mdiCloud } from "@mdi/js";
 
-const socialList = [
-  {
-    type: "github",
-    color: "black",
-    icon: mdiGithub,
-    label: `GitHub: YunYouJun`,
-    href: `https://github.com/YunYouJun`,
-  },
-  {
-    type: "telegram",
-    color: "#1da1f2",
-    icon: mdiTelegram,
-    label: "Telegram Channel",
-    href: "https://t.me/elpsycn",
-  },
-  {
-    type: "weibo",
-    color: "#DB2828",
-    icon: mdiSinaWeibo,
-    label: "微博：机智的云游君",
-    href: "http://weibo.com/jizhideyunyoujun",
-  },
-  {
-    type: "twitter",
-    color: "#1da1f2",
-    icon: mdiTwitter,
-    label: "Twitter: YunYouJun",
-    href: "https://twitter.com/YunYouJun",
-  },
-  {
-    type: "wechat",
-    color: "#1AAD19",
-    icon: mdiWechat,
-    label: "微信公众号：云游君",
-    href: "https://cdn.jsdelivr.net/gh/YunYouJun/cdn/img/about/white-qrcode-and-search.jpg",
-  },
+#const socialList = [
+
   {
     type: "blog",
     color: "#6435C9",
     icon: mdiEarth,
-    label: "博客：yunyoujun.cn",
-    href: "http://www.yunyoujun.cn",
+    label: "博客：blog.nomy.ga(建议使用移动的网络)",
+    href: "https://blog.nomy.ga",
   },
 ];
 
-function Copyright() {
-  return (
-    <div>
-      <Box>
-        <Typography variant="body2" color="textSecondary" align="center">
-          {"© "}
-          <Link color="inherit" href={pkg.repository.url} target="_blank">
-            Yun Air Conditioner
-          </Link>
-          <IconButton
-            sx={{ color: "#0078e7" }}
-            href="https://sponsors.yunyoujun.cn"
-            target="_blank"
-          >
-            <Icon path={mdiCloud} size={0.6}></Icon>
-          </IconButton>
-          <Link color="inherit" href={pkg.author.url} target="_blank">
-            {pkg.author.name}
-          </Link>
-        </Typography>
-      </Box>
-      <Typography variant="body2" color="textSecondary" align="center">
-        {" 2019 - " + new Date().getFullYear()}
-      </Typography>
-      <Box style={{ textAlign: "center" }}>
-        {socialList.map((item) => (
-          <Tooltip title={item.label} arrow>
-            <IconButton
-              sx={{ color: item.color }}
-              href={item.href}
-              target="_blank"
-            >
-              <Icon path={item.icon} size={1} />
-            </IconButton>
-          </Tooltip>
-        ))}
-      </Box>
-    </div>
-  );
-}
 
 export default Copyright;
